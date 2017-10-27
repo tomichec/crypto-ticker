88750ab846eff8f12efa9a79a9c2cc2513ce865d
Tomas <tomichec@gmail.com>
2017-10-27 19:55:08 +0200

    added a homepage with a list of selected cryptocurrencies and a form to add new ones

--- app.js
+++ app.js
@@ -2,10 +2,29 @@ const express = require('express');
 const app = express();
 const request = require('request');
 
+// from express 4.0 the body-parser needs to be installed separately and then invoked as follows
+// https://stackoverflow.com/questions/9304888/how-to-get-data-passed-from-a-form-in-express-node-js/38763341#38763341
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 app.set("view engine", "ejs");
 
+
+var currencies = ["bitcoin", "litecoin", "monero"];
+
 app.get('/', function (req, res) {
-    res.send('Hi there, welcome to crypto ticker!');
+    // res.send('Hi there, welcome to crypto ticker!');
+    res.render("home", {curList: currencies});    
+});
+
+app.post('/addCrypto', function (req, res) {
+    var newCrypto = req.body.newCrypto;
+    currencies.push(newCrypto);
+    console.log('New cryptocurrency was added.');
+    res.redirect('/');
 });
 
 app.get("/results", function (req, res) {
